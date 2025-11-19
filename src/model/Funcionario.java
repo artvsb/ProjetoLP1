21a8531bb2b2a0224c53ce20bcb1163d5dd499fe
Lucas Aguiar <llucasaguiar16@gmail.com>
2025-11-19 14:01:11 -0300

    att

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,4 +1,42 @@
 package model;
+import enums.CARGO;
 
-public class Funcionario {
+public class Funcionario extends Pessoa {
+    private String especialidade;
+    public CARGO cargo;
+
+    public Funcionario() {
+
+    }
+
+    public Funcionario(String nome, String login, String senha, int telefone, String especialidade){
+        super(nome, login, senha, telefone);
+        this.especialidade = especialidade;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public CARGO getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(CARGO cargo) {
+        this.cargo = cargo;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "especialidade='" + especialidade + '\'' +
+                ", cargo=" + cargo +
+                '}';
+    }
 }
--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,4 +1,62 @@
 package model;
 
-public class Pessoa {
+public abstract class Pessoa {
+    protected String nome;
+    protected String login;
+    protected String senha;
+    protected int telefone;
+
+
+    public Pessoa() {
+
+    }
+
+    public Pessoa(String nome, String login, String senha, int telefone) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", login='" + login + '\'' +
+                ", senha='" + senha + '\'' +
+                ", telefone=" + telefone +
+                '}';
+    }
 }
