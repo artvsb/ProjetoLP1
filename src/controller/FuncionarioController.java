937c0e74b8a5b561fd4c931057895b0973813d9d
Lucas Aguiar <llucasaguiar16@gmail.com>
2025-11-23 16:35:08 -0300

    att

--- src/controller/FuncionarioController.java
+++ src/controller/FuncionarioController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import model.Funcionario;
+import service.FuncionarioService;
+import java.util.List;
+
 public class FuncionarioController {
+    private FuncionarioService funcionarioService = new FuncionarioService();
+
+    public void cadastrarFuncionario(Funcionario f) {
+        funcionarioService.cadastrarFuncionario();
+    }
+    public List<Funcionario> listarFuncionarios() {
+        return funcionarioService.listarFuncionarios();
+    }
+    public void atualizarFuncionario(String nome, String novoNome) {
+        funcionarioService.atualizarFuncionario(nome, novoNome);
+    }
+    public void deletarFuncionario(String nome) {
+        funcionarioService.deletarFuncionario(nome);
+    }
 }
