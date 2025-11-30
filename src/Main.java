e2f3e502ae9c592c33fc3c175f00c5acde383b97
Lucas Aguiar <llucasaguiar16@gmail.com>
2025-11-30 18:39:47 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+import controller.*;
+import service.*;
+import model.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        Administrador a = new Administrador();
+        Cliente c = new Cliente();
+        ItemCardapio ic = new ItemCardapio();
+        ItemPedido ip = new ItemPedido();
+        Mesa m = new Mesa();
+        Pedido p = new Pedido();
+        Restaurante r = new Restaurante();
 
 
     }
