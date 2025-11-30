6733ec1ed8f5f0ceaac097ca94be12aa4a5eab2a
Lucas Aguiar <llucasaguiar16@gmail.com>
2025-11-30 19:36:14 -0300

    Add files via upload

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -22,7 +22,15 @@ public abstract class Pessoa {
 		this.email = email;
     }
 
-	public String getEmail() {
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
 		return email;
 	}
 
